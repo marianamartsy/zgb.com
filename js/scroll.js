3793969147c665641be335404fea9bed28e87780
marianamartsy <mart7mariana@gmail.com>
2024-05-15 12:41:52 +0200

    hero section edited

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,25 @@
-window.addEventListener('scroll', function () {
-	const aboutusSection = document.getElementById('aboutus')
-	const aboutusBounding = aboutusSection.getBoundingClientRect()
-	const aboutusImg = document.querySelector('.aboutus__img')
+const aboutusSection = document.getElementById('aboutus')
+const aboutusImg = document.querySelector('.aboutus__img')
+const heroSection = document.querySelector('.hero-img')
+const heroImg = document.querySelector('.hero-img__mobile')
 
+window.addEventListener('scroll', function () {
+	const aboutusBounding = aboutusSection.getBoundingClientRect()
 	if (aboutusBounding.top < window.innerHeight && aboutusBounding.bottom >= 0) {
 		aboutusImg.classList.add('animate-appear')
 	} else {
 		aboutusImg.classList.remove('animate-appear')
+	}
+})
+window.addEventListener('scroll', function () {
+	const heroBounding = heroSection.getBoundingClientRect()
+	if (window.innerWidth <= 992) {
+		if (heroBounding.top < window.innerHeight && heroBounding.bottom >= 0) {
+			heroImg.classList.add('animate-pulse')
+		} else {
+			heroImg.classList.remove('animate-pulse')
+		}
+	} else {
+		heroImg.classList.remove('animate-pulse')
 	}
 })
