11444341462eea6c96324df2b6fd2d9a692820a5
marianamartsy <mart7mariana@gmail.com>
2024-05-15 12:25:43 +0200

    hero section edited

--- js/scroll.js
+++ js/scroll.js
@@ -8,16 +8,4 @@ window.addEventListener('scroll', function () {
 	} else {
 		aboutusImg.classList.remove('animate-appear')
 	}
-
-	const heroSection = document.querySelector('.hero-img')
-	const heroBounding = heroSection.getBoundingClientRect()
-	const scrollPosition = window.scrollY
-	if (window.innerWidth <= 992) {
-		if (heroBounding.top < window.innerHeight && heroBounding.bottom >= 0) {
-			const parallaxValue = scrollPosition * 0.4
-			heroSection.style.backgroundPositionY = `calc(80% + ${parallaxValue}px)`
-		}
-	} else {
-		heroSection.style.backgroundPositionY = '50%'
-	}
 })
