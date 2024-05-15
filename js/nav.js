63f63a179e894715010307b57052169f696e1d83
marianamartsy <mart7mariana@gmail.com>
2024-05-15 22:06:22 +0200

    work clean air page edited

--- js/nav.js
+++ js/nav.js
@@ -3,24 +3,26 @@ const navBtn = document.querySelector('.burger-btn')
 const navItems = document.querySelectorAll('.nav-mobile__item')
 const menuItems = document.querySelectorAll('.nav-desktop__item')
 
-const navActive = () => {
+const navActiveMobile = () => {
 	nav.classList.toggle('active')
-	navItems.forEach(item => {
-		item.addEventListener('click', () => {
-			nav.classList.remove('active')
-		})
+}
+navBtn.addEventListener('click', navActiveMobile)
+
+document.addEventListener('DOMContentLoaded', function () {
+	const currentLocation = location.href
+
+	menuItems.forEach(item => {
+		if (item.href === currentLocation) {
+			item.classList.add('nav-desktop__item-active')
+		}
 	})
 
-	handleNavItemsAnimation()
-}
-
-menuItems.forEach(function (item) {
-	item.addEventListener('click', function () {
-		menuItems.forEach(function (item) {
-			item.classList.remove('nav-desktop__item-active')
+	menuItems.forEach(function (item) {
+		item.addEventListener('click', function () {
+			menuItems.forEach(function (item) {
+				item.classList.remove('nav-desktop__item-active')
+			})
+			item.classList.add('nav-desktop__item-active')
 		})
-		item.classList.add('nav-desktop__item-active')
 	})
 })
-
-navBtn.addEventListener('click', navActive)
