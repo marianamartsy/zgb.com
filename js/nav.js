6bb9dee2b3ef85c55a75797e490db856e634dae0
marianamartsy <mart7mariana@gmail.com>
2024-06-20 16:49:38 +0200

    styles edited

--- js/nav.js
+++ js/nav.js
@@ -13,28 +13,19 @@ const navActiveMobile = () => {
 }
 navBtn.addEventListener('click', navActiveMobile)
 
-document.addEventListener('DOMContentLoaded', function () {
-	const currentPath = getPathname(location.href)
-	const menuItems = document.querySelectorAll('.nav-desktop__item a')
+const currentLocation = location.href
 
-	menuItems.forEach(item => {
-		const itemPath = getPathname(item.href)
-		if (itemPath === currentPath) {
-			item.classList.add('nav-desktop__item-active')
-		}
-	})
-
-	menuItems.forEach(function (item) {
-		item.addEventListener('click', function () {
-			menuItems.forEach(function (item) {
-				item.classList.remove('nav-desktop__item-active')
-			})
-			item.classList.add('nav-desktop__item-active')
-		})
-	})
-
-	function getPathname(url) {
-		const pathname = new URL(url).pathname
-		return pathname.endsWith('/') ? pathname.slice(0, -1) : pathname
+menuItems.forEach(item => {
+	if (item.href === currentLocation) {
+		item.classList.add('nav-desktop__item-active')
 	}
+})
+
+menuItems.forEach(function (item) {
+	item.addEventListener('click', function () {
+		menuItems.forEach(function (item) {
+			item.classList.remove('nav-desktop__item-active')
+		})
+		item.classList.add('nav-desktop__item-active')
+	})
 })
