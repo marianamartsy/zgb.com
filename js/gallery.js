d6b1c953a9ba95d3b36784f9a805209588478906
marianamartsy <mart7mariana@gmail.com>
2024-06-25 18:40:34 +0200

    map to contacts added

--- js/gallery.js
+++ js/gallery.js
@@ -8,9 +8,23 @@ $(document).ready(function () {
 		mobileFirst: true,
 		responsive: [
 			{
+				breakpoint: 500,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			},
+			{
 				breakpoint: 700,
 				settings: {
 					slidesToShow: 3,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 4,
 					slidesToScroll: 1,
 				},
 			},
