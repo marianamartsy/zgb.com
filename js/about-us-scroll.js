ef4373ca77cf9de3f6d5535001cc1550d3c2b54b
marianamartsy <mart7mariana@gmail.com>
2024-06-25 19:41:24 +0200

    aboutus js edited

--- js/about-us-scroll.js
+++ js/about-us-scroll.js
@@ -1,7 +1,8 @@
+const aboutusBox = document.querySelector('.aboutus__box')
 const aboutusImg = document.querySelector('.aboutus__img')
 
 window.addEventListener('scroll', function () {
-	const aboutusBounding = aboutusImg.getBoundingClientRect()
+	const aboutusBounding = aboutusBox.getBoundingClientRect()
 	if (aboutusBounding.top < window.innerHeight && aboutusBounding.bottom >= 0) {
 		aboutusImg.classList.add('animate-appear')
 	} else {
