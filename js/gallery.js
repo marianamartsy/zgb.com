e355b1cc787408a598035d8f0e5fd90440511bb3
marianamartsy <mart7mariana@gmail.com>
2024-07-05 12:28:53 +0200

    photos added

--- js/gallery.js
+++ js/gallery.js
@@ -8,23 +8,16 @@ $(document).ready(function () {
 		mobileFirst: true,
 		responsive: [
 			{
-				breakpoint: 500,
+				breakpoint: 600,
 				settings: {
 					slidesToShow: 2,
 					slidesToScroll: 1,
 				},
 			},
 			{
-				breakpoint: 700,
+				breakpoint: 1000,
 				settings: {
 					slidesToShow: 3,
-					slidesToScroll: 1,
-				},
-			},
-			{
-				breakpoint: 1200,
-				settings: {
-					slidesToShow: 4,
 					slidesToScroll: 1,
 				},
 			},
