1898d9891cbf8125ce5e264cc60aae1ad2b89cbe
marianamartsy <mart7mariana@gmail.com>
2024-07-16 13:30:57 +0200

    contact form edited

---
+++ js/contact-form.js
@@ -0,0 +1,39 @@
+const contactForm = document.getElementById('contactForm')
+const fullNameInput = document.getElementById('full-name')
+const telephoneInput = document.getElementById('telephone')
+const emailInput = document.getElementById('email-address')
+const messageInput = document.getElementById('message')
+
+function handleSubmitForm(e) {
+	e.preventDefault()
+
+	const formData = {
+		name: fullNameInput.value,
+		telephone: telephoneInput.value,
+		email: emailInput.value,
+		message: messageInput.value,
+	}
+
+	fetch('zgb-back-227caenyw-marianamartsys-projects.vercel.app/api/contact', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(formData),
+	})
+		.then(response => response.json())
+		.then(data => {
+			if (data.success) {
+				alert('Wiadomość została wysłana!')
+				contactForm.reset()
+			} else {
+				alert('Wystąpił błąd. Spróbuj ponownie.')
+			}
+		})
+		.catch(error => {
+			console.error('Error:', error)
+			alert('Wystąpił błąd. Spróbuj ponownie.')
+		})
+}
+
+contactForm.addEventListener('submit', handleSubmitForm)
